38ce1fabe185a552571f4f73eb8d3a650779c30d
Yel-59 <rokomari144@gmail.com>
2021-09-29 23:37:26 +0600

    added model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    author_img = models.ImageField(upload_to="media")
+    email = models.EmailField()
+    institution = models.CharField(max_length=100 , blank=True,null=True)
+    study_subject = models.CharField(max_length=100, blank=True, null=True)
+    current_year = models.CharField(max_length=50, blank=True, null=True)
+    github = models.CharField(max_length=100, blank=True, null=True)
+    facebook = models.CharField(max_length=100, blank=True, null=True)
+    Twitter = models.CharField(max_length=100, blank=True, null=True)
+    def __str__(self):
+        return self.name
+class Comment(models.Model):
+    comment_body = models.CharField(max_length=500)
+    react = models.CharField(max_length=100, blank=True, null=True)
+    comment_reply = models.CharField(max_length=500)
+    comment_time_created = models.DateTimeField(auto_now_add=True)
+    comment_author = models.ForeignKey(Author, on_delete=SET_NULL, null=True)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField(max_length=500)
+    img = models.ImageField(upload_to="media")
+    author = models.ForeignKey(Author, on_delete=SET_NULL,null=True)
+    time_created = models.DateTimeField( auto_now_add=True)
+    react = models.CharField(max_length=100, blank=True, null=True)
+    comment = models.ManyToManyField(Comment)
+
+    
+
+
+    
+        
