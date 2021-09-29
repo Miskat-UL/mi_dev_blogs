c2fc6dd040306b47f0babac05f33dd224f210027
showmik <77013640+Miskat-UL@users.noreply.github.com>
2021-09-29 22:11:05 +0600

    upp

--- blogs/views.py
+++ blogs/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'blogs/index2.html')
-
+    
 
 def home2(request):
     return render(request, 'blogs/single_blog_page.html')
