a9ef47135889b51c321bd3cb26cc88f867c5d4b4
Yel-59 <rokomari144@gmail.com>
2021-10-06 15:12:50 +0600

    authorpage

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,5 +4,6 @@ from blogs import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('news/<int:pk>', views.home2, name='news'),
-    path('likes/<int:pk>', views.get_likes, name='likes')
+    path('likes/<int:pk>', views.get_likes, name='likes'),
+    path('author/<int:pk>', views.home3 ,name='author'),
 ]
