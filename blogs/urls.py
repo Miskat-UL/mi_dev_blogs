de0fa3fbeba632104f09099a4d8bc7b1992f4bb4
showmik <77013640+Miskat-UL@users.noreply.github.com>
2021-10-06 15:50:47 +0600

    up

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('news/<int:pk>', views.home2, name='news'),
     path('likes/<int:pk>', views.get_likes, name='likes'),
+
+    path('author/<int:pk>', views.home3 ,name='author'),
+
     path('write_blog/', views.blog_write, name="blog_create")
 ]
